fd6eb778053db6227dd9ac1e4393cb9d5a17b3b4
dappaardene <46179530+dappaardene@users.noreply.github.com>
2022-10-02 18:20:09 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -32,7 +32,7 @@ resource "azurerm_resource_group" "terraform101" {
 
 # Create a virtual network in the production-resources resource group
 resource "azurerm_virtual_network" "terraform_network" {
-  name                = "production-network"
+  name                = "terraform-network01"
   resource_group_name = azurerm_resource_group.terraform101.name
   location            = azurerm_resource_group.terraform101.location
   address_space       = ["10.0.0.0/16"]
