f4522bebf0fd8f87404521553e27b5027874988e
dappaardene <46179530+dappaardene@users.noreply.github.com>
2022-10-02 18:10:13 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -31,7 +31,7 @@ resource "azurerm_resource_group" "terraform101" {
 }
 
 # Create a virtual network in the production-resources resource group
-resource "azurerm_virtual_network" "terraform network" {
+resource "azurerm_virtual_network" "terraform_network" {
   name                = "production-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
